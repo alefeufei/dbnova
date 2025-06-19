d0d4d6265cf9a5f1b9c69d9f1530ed915e8e3fcd
Alexandre Cruv <alefeufei@gmail.com>
2025-06-19 11:20:05 -0300

    primeiro commit
    projeto de site dbnova, que terá apenas a home e contatos

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import { Header } from "@/components/header";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`$antialiased`}>
+          <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <header className="bg-white shadow-md p-6">
+      <nav className="flex justify-between items-center">
+      
+        <div>
+          <Image src="/logo.png" alt="DBNova Logo" width={150} height={50} />
+        </div>
+
+       
+        <ul className="flex space-x-6">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/solucoes">Soluções</a>
+          </li>
+          <li>
+            <a href="/quemsomos">Quem Somos</a>
+          </li>
+          <li>
+            <a href="/novidades">Novidades</a>
+          </li>
+          <li>
+            <a href="/trabalheconosco">Trabalhe Conosco</a>
+          </li>
+          <li>
+            <a href="/suporte">Suporte</a>
+          </li>
+          <li>
+            <a href="/contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
